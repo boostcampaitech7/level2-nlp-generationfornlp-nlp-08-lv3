ea6aa4f0bc6011b51a9b02aa0af061976edca6aa
beaver.zip <95961223+beaver-zip@users.noreply.github.com>
2024-11-29 17:51:12 +0900

    Lint utils.py

--- wrapup/code/utils.py
+++ wrapup/code/utils.py
@@ -1,12 +1,14 @@
-from peft import LoraConfig
 import json
 
+from peft import LoraConfig
+
+
 def get_peft_config():
-    # config 파일 로드
+    # Load config file
     with open("config.json", "r") as f:
         config = json.load(f)
-    
-    # LoRA 설정
+
+    # LoRA settings
     if config.get("use_lora", False):
         peft_config = LoraConfig(
             r=config["lora_r"],
@@ -14,7 +16,7 @@ def get_peft_config():
             lora_dropout=config["lora_dropout"],
             target_modules=config["lora_target_modules"],
             bias=config["lora_bias"],
-            task_type=config["lora_task_type"]
+            task_type=config["lora_task_type"],
         )
         return peft_config
     return None
